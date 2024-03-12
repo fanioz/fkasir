7d8bbc10620f88865441a2099826bfc9dcfa026a
fanioz <fanio.zilla@gmail.com>
2024-03-12 12:07:18 +0700

    Add Dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //
+    public function index()
+    {
+        $data = array(
+            'title' => 'Dashboard'
+        );
+        return view('dashboard.index', $data);
+    }
+}
