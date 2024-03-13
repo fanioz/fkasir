d4076a8e950b22cda251fbb65b8ec4c8305b6a02
fanioz <fanio.zilla@gmail.com>
2024-03-13 16:34:03 +0700

    Add authorization

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Models\Transaksi;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\DiskonController;
-use App\Http\Controllers\JenisBarangController;
 use App\Http\Controllers\TransaksiController;
-use App\Models\Transaksi;
+use App\Http\Controllers\JenisBarangController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,50 +26,52 @@ use App\Models\Transaksi;
 //Route::get('/', [AuthController::class,'index']);
 //Route::post('/ceklogin', [AuthController::class,'cek_login']);
 //Route::get('/logout', [AuthController::class,'logout']);
-
+Route::group(['middleware' => ['auth', 'checkRole:admin,kasir']], function () {
+  Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+});
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('login', [AuthController::class, 'customLogin'])->name('login.custom');
+Route::get('register', [AuthController::class, 'registration'])->name('register');
+Route::post('register', [AuthController::class, 'customRegistration'])->name('register.custom');
+Route::get('logout', [AuthController::class, 'signOut'])->name('signout');
 //Temporary Role Controller
-/* 
-Route::group(['middleware' => ['auth', 'checkRole:admin']], function(){
-*/
 
-//User
-Route::get('/user', [UserController::class, 'index']);
-Route::post('/user/store', [UserController::class, 'store']);
-Route::post('/user/update/{id}', [UserController::class, 'update']);
-Route::get('/user/destroy/{id}', [UserController::class, 'destroy']);
+Route::group(['middleware' => ['auth', 'checkRole:admin']], function () {
 
-//Jenis Barang
-Route::get('/jenisbarang', [JenisBarangController::class, 'index']);
-Route::post('/jenisbarang/store', [JenisBarangController::class, 'store']);
-Route::post('/jenisbarang/update/{id}', [JenisBarangController::class, 'update']);
-Route::get('/jenisbarang/destroy/{id}', [JenisBarangController::class, 'destroy']);
 
-//Data Barang
-Route::get('/barang', [BarangController::class, 'index']);
-Route::post('/barang/store', [BarangController::class, 'store']);
-Route::post('/barang/update/{id}', [BarangController::class, 'update']);
-Route::get('/barang/destroy/{id}', [BarangController::class, 'destroy']);
+  //User
+  Route::get('/user', [UserController::class, 'index']);
+  Route::post('/user/store', [UserController::class, 'store']);
+  Route::post('/user/update/{id}', [UserController::class, 'update']);
+  Route::get('/user/destroy/{id}', [UserController::class, 'destroy']);
 
-//Set diskon
-Route::get('/setdiskon', [DiskonController::class, 'index']);
-Route::post('/setdiskon/update/{id}', [DiskonController::class, 'update']);
+  //Jenis Barang
+  Route::get('/jenisbarang', [JenisBarangController::class, 'index']);
+  Route::post('/jenisbarang/store', [JenisBarangController::class, 'store']);
+  Route::post('/jenisbarang/update/{id}', [JenisBarangController::class, 'update']);
+  Route::get('/jenisbarang/destroy/{id}', [JenisBarangController::class, 'destroy']);
 
-/*
+  //Data Barang
+  Route::get('/barang', [BarangController::class, 'index']);
+  Route::post('/barang/store', [BarangController::class, 'store']);
+  Route::post('/barang/update/{id}', [BarangController::class, 'update']);
+  Route::get('/barang/destroy/{id}', [BarangController::class, 'destroy']);
+
+  //Set diskon
+  Route::get('/setdiskon', [DiskonController::class, 'index']);
+  Route::post('/setdiskon/update/{id}', [DiskonController::class, 'update']);
 });
 
-Route::group(['middleware' => ['auth', 'checkRole:admin,kasir']], function(){
-*/
-Route::get('/', [HomeController::class, 'index']);
+Route::group(['middleware' => ['auth', 'checkRole:admin,kasir']], function () {
 
-//Set Profile
-Route::get('/profile', [UserController::class, 'profile']);
-Route::post('/profile/updateprofile/{id}', [UserController::class, 'updateprofile']);
+  Route::get('/', [HomeController::class, 'index']);
+
+  //Set Profile
+  Route::get('/profile', [UserController::class, 'profile']);
+  Route::post('/profile/updateprofile/{id}', [UserController::class, 'updateprofile']);
 
 
-//Data Transaksi
-Route::get('/transaksi', [TransaksiController::class, 'index']);
-Route::get('/transaksicreate', [TransaksiController::class, 'create']);
-
-/*
+  //Data Transaksi
+  Route::get('/transaksi', [TransaksiController::class, 'index']);
+  Route::get('/transaksicreate', [TransaksiController::class, 'create']);
 });
- */
