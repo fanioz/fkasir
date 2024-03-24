30413210d52141b18820f8b6b2de7f78f8ae6dae
fanioz <fanio.zilla@gmail.com>
2024-03-24 18:29:06 +0700

    Add Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,63 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //seed user
+        $this->call([
+            UserSeeder::class,
+            //PelangganSeeder::class,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+
+        //seed jenis barang
+        DB::table('tbl_jenis_barang')->insert([
+            'nama_jenis'     => 'makanan',
+        ]);
+        DB::table('tbl_jenis_barang')->insert([
+            'nama_jenis'     => 'minuman',
+        ]);
+
+        //seed data barang
+        DB::table('tbl_barang')->insert([
+            'id_jenis'       => 1,
+            'nama_barang'    => 'rawon',
+            'harga'          => 30000,
+            'stok'           => 10,
+        ]);
+        DB::table('tbl_barang')->insert([
+            'id_jenis'       => 1,
+            'nama_barang'    => 'gulai',
+            'harga'          => 40000,
+            'stok'           => 10,
+        ]);
+        DB::table('tbl_barang')->insert([
+            'id_jenis'       => 2,
+            'nama_barang'    => 'kopi',
+            'harga'          => 10000,
+            'stok'           => 100,
+        ]);
+        DB::table('tbl_barang')->insert([
+            'id_jenis'       => 2,
+            'nama_barang'    => 'teh',
+            'harga'          => 5000,
+            'stok'           => 100,
+        ]);
+
+        //seed diskon
+        DB::table('tbl_diskon')->insert([
+            'diskon'         => 10,
+        ]);
+
+        //seed transaksi
+        DB::table('tbl_transaksi')->insert([
+            'id_diskon'      => 1,
+            'id_user'        => 1,
+            'id_barang'      => 1,
+            'qty'            => 1,
+            'total_harga'    => 30000,
+            'status'         => 'lunas',
+            'created_at'     => now(),
+            'updated_at'     => now(),
+        ]);
     }
 }
